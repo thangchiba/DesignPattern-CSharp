fab6039fd0e418e283eb56251637268f49d5f9ad
ThangChiba <thangchiba@gmail.com>
2022-10-06 18:54:29 +0900

    Prototype Pattern

---
+++ DesignPattern/PrototypePattern/Character/Minion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace PrototypePattern
+{
+    public class Minion
+    {
+        private int id { get; set; }
+
+        public Minion(int id)
+        {
+            this.id = id;
+        }
+
+        public Minion DeepCopy()
+        {
+            Minion clone = (Minion)this.MemberwiseClone();
+            clone.id = this.id + 1;
+            return clone;
+        }
+
+        public Minion ShallowCopy()
+        {
+            return (Minion)this.MemberwiseClone();
+        }
+
+        public override string? ToString()
+        {
+            return "My id is " + id;
+        }
+    }
+}
+
---
+++ DesignPattern/PrototypePattern/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PrototypePattern
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Minion minion1 = new Minion(1);
+            Minion minion2 = minion1.DeepCopy();
+            Minion minion3 = minion1.ShallowCopy();
+            Console.WriteLine(minion1);
+            Console.WriteLine(minion2);
+            Console.WriteLine(minion3);
+        }
+    }
+}
